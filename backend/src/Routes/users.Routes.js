b9c9599531f598ef5938d3a57ceab5a2864315be
Gadhe-Sai-prasad <154505332+Gadhe-Sai-prasad@users.noreply.github.com>
2026-01-26 23:25:14 +0530

    Clean up
    Removed unnecessary whitespace and ensured proper formatting.

--- backend/src/Routes/users.Routes.js
+++ backend/src/Routes/users.Routes.js
@@ -2,10 +2,8 @@
 import { login, register } from "../Controllers/userController.js";
 
 const router = express.Router();
-
 router.post("/login", login);
 router.post("/register", register);
-
 router.post("/add_to_activity",(req,res) => {
     res.json({message:"route not implemented yet"});
 });
